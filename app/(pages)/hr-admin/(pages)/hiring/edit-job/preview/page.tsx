a79f714deaaee79a82acc2853e86968f1a709287
mohdnas0001 <mohdnas0001@gmail.com>
2024-11-21 07:00:10 +0100

    create job --- edit job

---
+++ app/(pages)/hr-admin/(pages)/hiring/edit-job/preview/page.tsx
@@ -0,0 +1,7 @@
+import EditJobPreview from '@/app/_components/hr-admin/pages/hiring/overview/form/edit-job/edit-job-preview';
+
+const EditJobPreviewPage = () => {
+  return <EditJobPreview />;
+};
+
+export default EditJobPreviewPage;
--- app/_components/hr-admin/pages/hiring/overview/header/index.tsx
+++ app/_components/hr-admin/pages/hiring/overview/header/index.tsx
@@ -1,20 +1,34 @@
 import React from "react";
+import { useRouter } from "next/navigation";
+
 
 const HiringOverviewHeader = () => {
 
+  const router = useRouter();
+
+  const handleCreateJobClick = () => {
+    router.push("/hr-admin/hiring/create-job");
+  };
+  
   return (
-    <header className="flex flex-row justify-between items-center  ">
-      <h1 className="text-xl md:text-2xl font-bold text-gray-900 md:mb-4 ">Overview</h1>
-      <div className="flex flex-row   items-center">
-        
+    <>
+      {/* Header Section */}
+      <header className="flex flex-row justify-between items-center">
+        <h1 className="text-xl md:text-2xl font-bold text-gray-900 md:mb-4">
+          Overview
+        </h1>
+        <div className="flex flex-row items-center">
+          <button
+            className="px-6 py-2 text-base font-semibold bg-[#0035C3] text-white rounded-lg hover:bg-blue-600 focus:outline-none"
+            onClick={handleCreateJobClick}
+          >
+            Create new job
+          </button>
+        </div>
+      </header>
 
-        <button className="px-6 py-2 text-base font-semibold bg-primary500 text-white rounded-lg hover:bg-blue-600 focus:outline-none ">
-          Create new job
-        </button>
-
-       
-      </div>
-    </header>
+    
+    </>
   );
 };
 
