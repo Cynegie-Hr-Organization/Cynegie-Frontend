8e594010c221aab4f37254f8993008aa3aa2c273
Peter Chisom <Peterchisom69@gmail.com>
2024-11-27 13:40:57 +0100

    fix: uncessary empty jsx syntaxes and extra margin in create payroll

--- app/_components/hr-admin/pages/payroll-management/pages/overview/create-payroll/index.tsx
+++ app/_components/hr-admin/pages/payroll-management/pages/overview/create-payroll/index.tsx
@@ -27,154 +27,152 @@ const HrAdminCreatePayrollPage = () => {
   );
 
   return (
-    <>
-      <Stack marginX={5} gap={3} mb={10}>
-        <Stack
-          direction='row'
-          alignItems='center'
-          gap={1}
-          sx={{ display: { xs: 'none', sm: 'flex' }, cursor: 'pointer' }}
-          onClick={() => router.push('/hr-admin/payroll/overview')}
-        >
-          <ChevronLeft
-            sx={{ color: '#8D8484', height: '36px', width: '36px' }}
-          />
-          <div style={{ color: '#667185', fontWeight: 400, fontSize: '16px' }}>
-            Back to Payroll Management
-          </div>
-        </Stack>
-        <Stack gap={2}>
-          <div className='section-heading'>Create Payroll</div>
-          <div className='common-card'>
-            <Grid2 spacing={2} container>
-              {[
-                { label: 'Payroll Name', placeholder: 'Enter' },
-                { label: 'Payroll Period', placeholder: 'Select period' },
-                { label: 'Payment Date', placeholder: 'Select date' },
-              ].map((item, index) => (
-                <Grid2 key={index} size={{ xs: 12, md: 4 }}>
+    <Stack gap={3} mb={10}>
+      <Stack
+        direction='row'
+        alignItems='center'
+        gap={1}
+        sx={{ display: { xs: 'none', sm: 'flex' }, cursor: 'pointer' }}
+        onClick={() => router.push('/hr-admin/payroll/overview')}
+      >
+        <ChevronLeft
+          sx={{ color: '#8D8484', height: '36px', width: '36px' }}
+        />
+        <div style={{ color: '#667185', fontWeight: 400, fontSize: '16px' }}>
+          Back to Payroll Management
+        </div>
+      </Stack>
+      <Stack gap={2}>
+        <div className='section-heading'>Create Payroll</div>
+        <div className='common-card'>
+          <Grid2 spacing={2} container>
+            {[
+              { label: 'Payroll Name', placeholder: 'Enter' },
+              { label: 'Payroll Period', placeholder: 'Select period' },
+              { label: 'Payment Date', placeholder: 'Select date' },
+            ].map((item, index) => (
+              <Grid2 key={index} size={{ xs: 12, md: 4 }}>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 5,
+                  }}
+                >
                   <div
                     style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      gap: 5,
+                      color: '#101928',
+                      fontWeight: 600,
+                      fontSize: '14px',
                     }}
                   >
-                    <div
-                      style={{
-                        color: '#101928',
-                        fontWeight: 600,
-                        fontSize: '14px',
-                      }}
-                    >
-                      {item.label}
-                    </div>
-                    {index == 0 ? (
-                      // <TextField
-                      //   key={index}
-                      //   sx={{
-                      //     borderRadius: '6px',
-                      //     '&:hover': {
-                      //       border: '0.5px solid #3498FF',
-                      //     },
-                      //   }}
-                      //   inputProps={{
-                      //     style: {
-                      //       height: '3px',
-                      //       fontSize: '14px',
-                      //       fontWeight: 400,
-                      //     },
-                      //   }}
-                      //   placeholder={item.placeholder}
-                      // />
-                      <Input
-                        placeholder={item.placeholder}
-                        key={index}
-                        style={{ borderRadius: '6px' }}
-                      />
-                    ) : index == 1 ? (
-                      <DateRangePicker
-                        style={{
-                          borderRadius: '6px',
-                          // width: '40px',
-                          // padding: '0px'
-                        }}
-                        preventOverflow
-                        showOneCalendar
-                        cleanable={false}
-                        ranges={[]}
-                        format='dd MMM yyyy'
-                        placeholder={item.placeholder}
-                        onChange={(e) => {
-                          e && setDateRange({ startDate: e[0], endDate: e[1] });
-                        }}
-                        character=' – '
-                        caretAs={CalendarIcon}
-                      />
-                    ) : (
-                      <DatePicker
-                        key={index}
-                        placeholder={item.placeholder}
-                        style={{
-                          borderRadius: '6px',
-                        }}
-                        format='dd MMM yyyy'
-                        cleanable={false}
-                        caretAs={CalendarIcon}
-                      />
-                    )}
+                    {item.label}
                   </div>
-                </Grid2>
-              ))}
-            </Grid2>
-          </div>
-        </Stack>
-        <Stack gap={2}>
-          <div className='card-title-small'>
-            Select Employees for Payroll Cycle
-          </div>
-          <SelectEmployeesForPayrollTable
-            getSelectedRows={(rows) => setSelectedRows(rows)}
-          />
-        </Stack>
-        <Stack
-          direction='row'
-          alignItems='center'
-          justifyContent='flex-end'
-          gap={2}
-        >
-          <button
-            style={{
-              borderRadius: '8px',
-              border: '1.5px solid #98A2B3',
-              color: '#344054',
-              fontSize: '16px',
-              fontWeight: 600,
-              padding: '10px 0px',
-              width: '250px',
-              backgroundColor: '#FFFFFF',
-            }}
-          >
-            Save & Continue Later
-          </button>
-          <button
-            onClick={() => router.push('/hr-admin/payroll/review-payroll')}
-            style={{
-              borderRadius: '8px',
-              border: '1.5px solid #98A2B3',
-              color: selectedRows.length > 1 ? '#FFF' : '#344054',
-              fontSize: '16px',
-              fontWeight: 600,
-              padding: '10px 0px',
-              width: '250px',
-              backgroundColor: selectedRows.length > 1 ? '#0035C3' : '#98A2B3',
-            }}
-          >
-            Continue
-          </button>
-        </Stack>
+                  {index == 0 ? (
+                    // <TextField
+                    //   key={index}
+                    //   sx={{
+                    //     borderRadius: '6px',
+                    //     '&:hover': {
+                    //       border: '0.5px solid #3498FF',
+                    //     },
+                    //   }}
+                    //   inputProps={{
+                    //     style: {
+                    //       height: '3px',
+                    //       fontSize: '14px',
+                    //       fontWeight: 400,
+                    //     },
+                    //   }}
+                    //   placeholder={item.placeholder}
+                    // />
+                    <Input
+                      placeholder={item.placeholder}
+                      key={index}
+                      style={{ borderRadius: '6px' }}
+                    />
+                  ) : index == 1 ? (
+                    <DateRangePicker
+                      style={{
+                        borderRadius: '6px',
+                        // width: '40px',
+                        // padding: '0px'
+                      }}
+                      preventOverflow
+                      showOneCalendar
+                      cleanable={false}
+                      ranges={[]}
+                      format='dd MMM yyyy'
+                      placeholder={item.placeholder}
+                      onChange={(e) => {
+                        e && setDateRange({ startDate: e[0], endDate: e[1] });
+                      }}
+                      character=' – '
+                      caretAs={CalendarIcon}
+                    />
+                  ) : (
+                    <DatePicker
+                      key={index}
+                      placeholder={item.placeholder}
+                      style={{
+                        borderRadius: '6px',
+                      }}
+                      format='dd MMM yyyy'
+                      cleanable={false}
+                      caretAs={CalendarIcon}
+                    />
+                  )}
+                </div>
+              </Grid2>
+            ))}
+          </Grid2>
+        </div>
       </Stack>
-    </>
+      <Stack gap={2}>
+        <div className='card-title-small'>
+          Select Employees for Payroll Cycle
+        </div>
+        <SelectEmployeesForPayrollTable
+          getSelectedRows={(rows) => setSelectedRows(rows)}
+        />
+      </Stack>
+      <Stack
+        direction='row'
+        alignItems='center'
+        justifyContent='flex-end'
+        gap={2}
+      >
+        <button
+          style={{
+            borderRadius: '8px',
+            border: '1.5px solid #98A2B3',
+            color: '#344054',
+            fontSize: '16px',
+            fontWeight: 600,
+            padding: '10px 0px',
+            width: '250px',
+            backgroundColor: '#FFFFFF',
+          }}
+        >
+          Save & Continue Later
+        </button>
+        <button
+          onClick={() => router.push('/hr-admin/payroll/review-payroll')}
+          style={{
+            borderRadius: '8px',
+            border: '1.5px solid #98A2B3',
+            color: selectedRows.length > 1 ? '#FFF' : '#344054',
+            fontSize: '16px',
+            fontWeight: 600,
+            padding: '10px 0px',
+            width: '250px',
+            backgroundColor: selectedRows.length > 1 ? '#0035C3' : '#98A2B3',
+          }}
+        >
+          Continue
+        </button>
+      </Stack>
+    </Stack>
   );
 };
 
