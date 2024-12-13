245a847e26cc6b6dbbae55bb588b9dfe9fbc642b
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-12-13 17:53:29 +0100

    Update Performance Summary Chart

--- app/_components/employee/pages/performance-summary/chart/index.tsx
+++ app/_components/employee/pages/performance-summary/chart/index.tsx
@@ -1,22 +1,22 @@
 import { color } from '@/constants';
 import React from 'react';
-import { AreaChart, Area, Tooltip, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, Tooltip, ResponsiveContainer, XAxis } from 'recharts';
 
 const data = [
-  { name: 'Jan', ['2024']: 0, ['2023']: 0 },
-  { name: 'Feb', ['2024']: 3000, ['2023']: 2210 },
-  { name: 'Mar', ['2024']: 2000, ['2023']: 2290 },
-  { name: 'Apr', ['2024']: 2780, ['2023']: 2000 },
-  { name: 'May', ['2024']: 1890, ['2023']: 2181 },
-  { name: 'Jun', ['2024']: 2390, ['2023']: 2500 },
-  { name: 'Jul', ['2024']: 3490, ['2023']: 2100 },
+  { ['2024']: 0, ['2023']: 0 },
+  { ['2024']: 3000, ['2023']: 2210 },
+  { ['2024']: 2000, ['2023']: 2290 },
+  { ['2024']: 2780, ['2023']: 2000 },
+  { ['2024']: 1890, ['2023']: 2181 },
+  { ['2024']: 2390, ['2023']: 2500 },
+  { ['2024']: 3490, ['2023']: 2100 },
 ];
 
 const PerformanceSummaryChart = () => {
   return (
     <ResponsiveContainer width='100%' height={300}>
       <AreaChart data={data}>
-        <Tooltip />
+        {/* <Tooltip /> */}
         <Area
           type='linear'
           dataKey='2024'
