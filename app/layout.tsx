26561657adc1a82d48996c18964f849110ef8901
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-12-13 15:10:59 +0100

    Add Rsuite and Big Calendar Styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import QueryClientProvider from "@/contexts/ReactQueryProvider";
+import type { Metadata } from 'next';
+import './globals.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import QueryClientProvider from '@/contexts/ReactQueryProvider';
+import 'rsuite/dist/rsuite.min.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 export const metadata: Metadata = {
-  title: "Cynegie",
-  description: "HR admin flow dashboard",
+  title: 'Cynegie',
+  description: 'HR admin flow dashboard',
 };
 
 export default function RootLayout({
@@ -15,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased font-sans">
+    <html lang='en'>
+      <body className='antialiased font-sans'>
         <ToastContainer />
         <QueryClientProvider>{children}</QueryClientProvider>
       </body>
