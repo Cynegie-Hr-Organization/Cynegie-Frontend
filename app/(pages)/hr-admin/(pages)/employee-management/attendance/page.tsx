3ec3e4c3ded19191643c1a5611eb7174307995de
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-12-15 06:18:19 +0100

    Add Employee Nav Bar

---
+++ app/(pages)/hr-admin/(pages)/employee-management/attendance/page.tsx
@@ -0,0 +1,5 @@
+import HrAdminEmployeeAttendanceManagement from '@/app/_components/hr-admin/pages/employee-management/attendance';
+
+export default function HrAdminEmployeeAttendanceManagementPage() {
+  return <HrAdminEmployeeAttendanceManagement />;
+}
---
+++ app/(pages)/hr-admin/(pages)/employee-management/directory/page.tsx
@@ -0,0 +1,5 @@
+import HrAdminEmployeeDirectory from '@/app/_components/hr-admin/pages/employee-management/directory';
+
+export default function HrAdminEmployeeDirectoryPage() {
+  return <HrAdminEmployeeDirectory />;
+}
