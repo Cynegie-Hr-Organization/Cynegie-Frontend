02149573689102b4bc8e9a5046c68bd1ef1d2251
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-12-16 09:07:23 +0100

    Chore: Make Attendance Report Component Resusable

--- app/_components/hr-admin/pages/employee-management/attendance/bulk-report/index.tsx
+++ app/_components/hr-admin/pages/employee-management/attendance/bulk-report/index.tsx
@@ -1,58 +1,35 @@
 'use client';
-import Page from '@/app/_components/shared/page';
-import { ButtonType } from '@/app/_components/shared/page/heading/types';
-import CardGroup from '@/app/_components/shared/section-with-cards/card-group';
-import Table from '@/app/_components/shared/table';
 import { FieldType } from '@/app/_components/shared/table/types';
-import { AttendanceStatusMap, route } from '@/constants';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import DownloadReportModal from '../../../payroll-management/modals/download-report';
+import HrAdminEmployeeAttendanceManagementReport from '../report';
 
 const HrAdminEmployeeAttendanceManagementBulkReport = () => {
-  const router = useRouter();
-  const [openDownloadModal, setOpenDownloadModal] = useState(false);
   return (
-    <Page
-      backText='Back to Attendance Management'
-      onBackTextClick={() =>
-        router.push(route.hrAdmin.employeeManagement.attendanceManagement.home)
-      }
+    <HrAdminEmployeeAttendanceManagementReport
       title='Attendance Report (HR & IT Department: Oct 1 - Oct 15, 2024)'
-      hasButtons
-      rightButton={{
-        type: ButtonType.contained,
-        text: 'Download Report',
-        onClick: () => setOpenDownloadModal(true),
-      }}
-    >
-      <CardGroup
-        cards={[
-          {
-            labelText: 'Total Employee',
-            value: 120,
-            valueBelow: true,
-          },
-          {
-            labelText: 'Total Days Analyzed',
-            value: 15,
-            valueBelow: true,
-          },
-          {
-            labelText: 'Present Employees',
-            value: 80,
-            valueBelow: true,
-          },
-          {
-            labelText: 'Absent Employees',
-            value: 25,
-            valueBelow: true,
-          },
-        ]}
-        gridItemSize={{ xs: 12, sm: 6, md: 3 }}
-      />
-      <Table
-        headerRowData={[
+      cards={[
+        {
+          labelText: 'Total Employees',
+          value: 120,
+          valueBelow: true,
+        },
+        {
+          labelText: 'Total Days Analyzed',
+          value: 15,
+          valueBelow: true,
+        },
+        {
+          labelText: 'Present Employees',
+          value: 80,
+          valueBelow: true,
+        },
+        {
+          labelText: 'Absent Employees',
+          value: 25,
+          valueBelow: true,
+        },
+      ]}
+      tableProps={{
+        headerRowData: [
           'Employee Name',
           'Staff ID',
           'Department',
@@ -62,13 +39,13 @@ const HrAdminEmployeeAttendanceManagementBulkReport = () => {
           'Hours Worked',
           'Status',
           'Overtime Hours',
-        ]}
-        fieldTypes={[
+        ],
+        fieldTypes: [
           ...Array(7).fill(FieldType.text),
           FieldType.status,
           FieldType.text,
-        ]}
-        displayedFields={[
+        ],
+        displayedFields: [
           'name',
           'id',
           'department',
@@ -78,8 +55,8 @@ const HrAdminEmployeeAttendanceManagementBulkReport = () => {
           'hoursWorked',
           'status',
           'overtimeHours',
-        ]}
-        bodyRowData={Array(5).fill({
+        ],
+        bodyRowData: Array(5).fill({
           name: 'Ayomide Alibaba',
           id: 'CYN0235',
           department: 'Product',
@@ -89,23 +66,16 @@ const HrAdminEmployeeAttendanceManagementBulkReport = () => {
           hoursWorked: '8',
           status: 'Absent',
           overtimeHours: '8',
-        })}
-        statusMap={AttendanceStatusMap}
-        filters={[
+        }),
+        filters: [
           { name: 'Name', items: ['Emmanuel Okpara'] },
           { name: 'Department', items: ['Sales'] },
           { name: 'Job Title', items: ['Regional Manager'] },
           { name: 'Status', items: ['Present'] },
           { name: 'Date', items: ['Select Date'] },
-        ]}
-      />
-      {openDownloadModal && (
-        <DownloadReportModal
-          open={openDownloadModal}
-          onCloseFn={() => setOpenDownloadModal(false)}
-        />
-      )}
-    </Page>
+        ],
+      }}
+    />
   );
 };
 
--- app/_components/hr-admin/pages/employee-management/attendance/individual-report/index.tsx
+++ app/_components/hr-admin/pages/employee-management/attendance/individual-report/index.tsx
@@ -1,17 +1,67 @@
 'use client';
-import Page from '@/app/_components/shared/page';
-import { ButtonType } from '@/app/_components/shared/page/heading/types';
+import HrAdminEmployeeAttendanceManagementReport from '../report';
+import { FieldType } from '@/app/_components/shared/table/types';
 
 const HrAdminEmployeeAttendanceManagementIndividualReport = () => {
   return (
-    <Page
-      title='Attendance Managment'
-      hasButtons
-      rightButton={{
-        type: ButtonType.contained,
-        text: 'Generate Attendance Report',
+    <HrAdminEmployeeAttendanceManagementReport
+      title='Attendance Report for Emmanuel Okpara (Oct 15, 2024 - Oct 25, 2024)'
+      cards={[
+        {
+          labelText: 'Total Days Analyzed',
+          value: '15 Days',
+          valueBelow: true,
+        },
+        {
+          labelText: 'On Leave',
+          value: '0 Days',
+          valueBelow: true,
+        },
+        {
+          labelText: 'Present',
+          value: '10 Days',
+          valueBelow: true,
+        },
+        {
+          labelText: 'Absent',
+          value: '3 Days',
+          valueBelow: true,
+        },
+      ]}
+      tableProps={{
+        hasActionsColumn: false,
+        headerRowData: [
+          'Date',
+          'Check In Time',
+          'Clock-Out Time',
+          'Hours Worked',
+          'Status',
+          'Overtime Hours',
+        ],
+        fieldTypes: [
+          ...Array(4).fill(FieldType.text),
+          FieldType.status,
+          FieldType.text,
+        ],
+        displayedFields: [
+          'date',
+          'checkInTime',
+          'checkOutTime',
+          'hoursWorked',
+          'status',
+          'overtimeHours',
+        ],
+        bodyRowData: Array(5).fill({
+          date: 'Oct 1, 2024',
+          checkInTime: '9:02 AM',
+          checkOutTime: '4:56 PM',
+          hoursWorked: '8',
+          status: 'Absent',
+          overtimeHours: 'N/A',
+        }),
+        filters: [{ name: 'Status', items: ['Present'] }],
       }}
-    ></Page>
+    />
   );
 };
 
