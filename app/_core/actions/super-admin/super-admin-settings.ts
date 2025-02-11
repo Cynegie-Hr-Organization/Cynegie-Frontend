c5e04e3ebcdefaae8d54769fdc103288d32b2112
Peter Chisom <Peterchisom69@gmail.com>
2025-02-11 18:22:39 +0100

    chore: super-admin separation of concerns

--- app/_core/actions/super-admin/super-admin-settings.ts
+++ app/_core/actions/super-admin/super-admin-settings.ts
@@ -1,3 +1,4 @@
+import { ISuperAdminSettings } from "@/app/_core/interfaces/super-admin";
 import { handleError, Http } from "@/app/_core/utils/axios";
 import { headers } from "@/app/_core/utils/session";
 import { getSession } from "next-auth/react";
@@ -15,81 +16,4 @@ export const getSuperAdminSettings = async () => {
   } catch (error) {
     throw handleError(error)
   }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-export type ApprovalStage = {
-  stageName: string;
-  approvers: string[];
-};
-
-export type ApprovalWorkflowSettings = {
-  stages: ApprovalStage[];
-};
-
-export type UserAccessSecuritySettings = {
-  minPasswordLength: number;
-  passwordComplexity: 'low' | 'medium' | 'high';
-  sessionTimeout: number;
-};
-
-export type IntegrationSettings = {
-  api: string;
-  thirdPartyIntegrations: string[];
-  scheduleDataBackups: 'daily' | 'weekly' | 'monthly';
-  errorLogLevel: 'debug' | 'info' | 'warn' | 'error';
-};
-
-export type ComplianceSettings = {
-  complianceReminderFrequency: 'daily' | 'weekly' | 'monthly' | 'quarterly';
-  dataRetentionDuration: number;
-};
-
-export type CustomizationSettings = {
-  themeAndBranding: 'light' | 'dark' | 'system';
-  dashboardConfiguration: 'default' | 'compact' | 'detailed';
-};
-
-export type SystemSettings = {
-  approvalWorkflowSettings: ApprovalWorkflowSettings;
-  userAccessSecuritySettings: UserAccessSecuritySettings;
-  integrationSettings: IntegrationSettings;
-  complianceSettings: ComplianceSettings;
-  customizationSettings: CustomizationSettings;
-};
-
-export type NotificationType = 'email' | 'sms' | 'push' | 'none';
-
-export interface ISuperAdminSettings {
-  id: string;
-  name: string;
-  status: 'active' | 'inactive' | 'suspended';
-
-  createdAt: string;
-  updatedAt: string;
-
-  address: string;
-  email: string;
-  phone: string;
-  logo: string;
-
-  dateFormat: string;
-  timeZone: string;
-  supportedLanguages: string[];
-
-  notificationType: NotificationType;
-  enableSystemNotification: boolean;
-
-  settings: SystemSettings;
 }
---
+++ app/_core/interfaces/super-admin.ts
@@ -0,0 +1,66 @@
+
+export type TimeFrequency = 'daily' | 'weekly' | 'monthly' | 'quarterly';
+export type ErrorLevel = 'debug' | 'info' | 'warn' | 'error';
+export type PasswordComplexity = 'low' | 'medium' | 'high';
+export type AppThemeType = 'light' | 'dark' | 'system';
+export type DashboardLayout = 'default' | 'compact' | 'detailed';
+export type NotificationType = 'email' | 'sms' | 'push' | 'none';
+export type SuperAdminStatus = 'active' | 'inactive' | 'suspended';
+
+export type ApprovalStage = {
+  stageName: string;
+  approvers: string[];
+};
+
+export type ApprovalWorkflowSettings = {
+  stages: ApprovalStage[];
+};
+
+export type UserAccessSecuritySettings = {
+  minPasswordLength: number;
+  passwordComplexity: PasswordComplexity;
+  sessionTimeout: number;
+};
+
+export type IntegrationSettings = {
+  api: string;
+  thirdPartyIntegrations: string[];
+  scheduleDataBackups: TimeFrequency;
+  errorLogLevel: ErrorLevel;
+};
+
+export type ComplianceSettings = {
+  complianceReminderFrequency: TimeFrequency;
+  dataRetentionDuration: number;
+};
+
+export type CustomizationSettings = {
+  themeAndBranding: AppThemeType;
+  dashboardConfiguration: DashboardLayout;
+};
+
+export type SystemSettings = {
+  approvalWorkflowSettings: ApprovalWorkflowSettings;
+  userAccessSecuritySettings: UserAccessSecuritySettings;
+  integrationSettings: IntegrationSettings;
+  complianceSettings: ComplianceSettings;
+  customizationSettings: CustomizationSettings;
+};
+
+export interface ISuperAdminSettings {
+  id: string;
+  name: string;
+  status: SuperAdminStatus;
+  createdAt: string;
+  updatedAt: string;
+  address: string;
+  email: string;
+  phone: string;
+  logo: string;
+  dateFormat: string;
+  timeZone: string;
+  supportedLanguages: string[];
+  notificationType: NotificationType;
+  enableSystemNotification: boolean;
+  settings: SystemSettings;
+}
